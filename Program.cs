0f0fd4f12f4b5d24acd71652225ccec1301fe22f
gfloresuth <dexterminio@gmail.com>
2017-03-09 14:26:47 -0700

    Primeros archivos

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Graficas
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string []args)
+        {
+            Application.Run(new MainForm());
+        }
+    }
+}
---
+++ app
@@ -0,0 +1,5 @@
+-r:System.Windows.Forms.dll
+-r:System.Drawing.dll
+-out:bin/Graficas.exe
+Program.cs
+MainForm.cs
